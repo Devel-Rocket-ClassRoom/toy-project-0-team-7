02df11f3650e0c511a9d4bbcebf72adc4a63706b
jinag8904 <jinag8904@gmail.com>
2026-04-30 15:03:47 +0900

    LineManager - 노선 관리자

--- Assets/Scripts/Line/LineManager.cs
+++ Assets/Scripts/Line/LineManager.cs
@@ -1,16 +1,53 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LineManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Line linePrefab;
+    private Line line_onMaking;
+    public bool IsValidLine => line_onMaking.stations.Count < 2;
+
+    private LineRenderer lineRenderer;
+
+    private List<Line> lines;
+
+    public void StartLine(RaycastHit2D hit, Vector3 pos)
     {
-        
+        line_onMaking = Instantiate(linePrefab, transform);
+        line_onMaking.AddStationEnd(hit.collider.gameObject.GetComponent<Station>());
+
+        lineRenderer = line_onMaking.GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, pos);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void FixLine()
     {
-        
+        line_onMaking.Init();
+        AddLine(line_onMaking);
+        line_onMaking = null;
+    }
+
+    public void CancelLine()
+    {
+        Destroy(line_onMaking.gameObject);
+        line_onMaking = null;
+    }
+
+    public void UpdatePreviewPoint(Vector3 previewPoint)
+    {
+        lineRenderer.SetPosition(line_onMaking.stations.Count, previewPoint);
+    }
+
+    public void AddStationInMakingLine(Station station)
+    {
+        if (!line_onMaking.stations.Contains(station))
+        {
+            line_onMaking.AddStationEnd(station);
+        }
+    }
+
+    public void AddLine(Line line)
+    {
+        lines.Add(line);
     }
 }
--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -2,14 +2,12 @@ using UnityEngine;
 
 public class MouseInput : MonoBehaviour
 {
+    public enum Movable { None, Line, Train }
+
     private Camera cam;
     public LineManager lineManager;
 
-    public Line linePrefab;
-    private Line line_onMaking;
-    private LineRenderer lineRenderer;
-
-    private bool isHolding = false;
+    private Movable holding;
 
     void Start()
     {
@@ -27,60 +25,55 @@ public class MouseInput : MonoBehaviour
             {
                 if (hit.collider.CompareTag("Station"))
                 {
-                    Debug.Log("선 생성");
                     var pos = hit.collider.gameObject.transform.position;
                     pos.z = 0f;
 
-                    line_onMaking = Instantiate(linePrefab, lineManager.transform);
-                    line_onMaking.AddStationEnd(hit.collider.gameObject.GetComponent<Station>());
-
-                    isHolding = true;
-
-                    lineRenderer = line_onMaking.GetComponent<LineRenderer>();
-                    lineRenderer.SetPosition(0, pos);
+                    lineManager.StartLine(hit, pos);
+                    holding = Movable.Line;
+                    return;
                 }
             }
         }
 
-        if (isHolding)
+        if (holding != Movable.None)
         {
             if (Input.GetMouseButton(0))    // 드래그
             {
                 var previewPoint = point;
                 previewPoint.z = 0f;
-                lineRenderer.SetPosition(line_onMaking.stations.Count, previewPoint);
+
+                switch (holding)
+                {
+                    case Movable.Line:
+                        lineManager.UpdatePreviewPoint(previewPoint);
+                        break;
+                    case Movable.Train:
+                        break;
+                }
 
                 if (hit.collider != null)   // 해당 위치에 충돌한 오브젝트가 존재하는 경우
                 {
                     if (hit.collider.CompareTag("Station"))
                     {
                         var station = hit.collider.GetComponent<Station>();
-
-                        if (!line_onMaking.stations.Contains(station))
-                        {
-                            line_onMaking.AddStationEnd(station);
-                            Debug.Log($"선에 역 추가: {station.name}");
-                        }
+                        lineManager.AddStationInMakingLine(station);
                     }
                 }
             }
 
             if (Input.GetMouseButtonUp(0))  // 릴리즈
             {
-                if (line_onMaking.stations.Count < 2)
+                if (lineManager.IsValidLine)    // 유효한 선이면 확정
                 {
-                    Debug.Log($"미완성 선 삭제");
-                    Destroy(line_onMaking.gameObject);
-                    line_onMaking = null;
-                    isHolding = false;
-                    return;
+                    lineManager.FixLine();
                 }
 
-                // 선 확정
-                Debug.Log($"선 확정");
-                line_onMaking.Init();
-                line_onMaking = null;
-                isHolding = false;
+                else
+                {
+                    lineManager.CancelLine();
+                }
+
+                holding = Movable.None;                
             }
         }
     }
