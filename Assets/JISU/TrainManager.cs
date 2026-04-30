49c09ac1b80cf489945531171b4739a7bdc40cd7
윤지수 <yoonjisu1201@gmail.com>
2026-04-30 14:51:48 +0900

    [Feature] 열차 기본 이동 로직 구현
    #6

---
+++ Assets/JISU/TrainManager.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrainManager : MonoBehaviour
+{
+    public List<Train> activeTrains = new List<Train>();
+    //public Train train;
+    public GameObject trainPrefab;
+
+    // [수정 예정] 나중에 LineManager가 완성되면 testStations 대신 
+    // 실제 Line이 보유한 Station 리스트를 가져와야 함
+    public List<GameObject> testStations;
+
+    private void Start()
+    {
+        SpawnTrain();
+    }
+    private void Update()
+    {
+        //[체크] 일시정지 기능 추가시 여기서 체크
+        foreach (var train in activeTrains)
+        {
+            train.Move();
+        }
+    }
+    public void SpawnTrain()
+    {
+        //[수정 예정] 실제 게임에선 0번 역이 아니라 사용자가 클릭한 노선의 시작점에 스폰
+        GameObject trainObject = Instantiate(trainPrefab, testStations[0].transform.position, Quaternion.identity);
+        Train train = trainObject.GetComponent<Train>();
+
+        //[수정 예정] 실제 데이터로 변경
+        train.SetPath(testStations);
+        activeTrains.Add(train);
+    }
+}
