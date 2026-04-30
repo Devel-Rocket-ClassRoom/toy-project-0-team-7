d2eecd92695085f8c2b714c47ab8f7c6ab62eff3
jinag8904 <jinag8904@gmail.com>
2026-04-30 13:39:52 +0900

    마우스 입력, 노선 그리기(기본)
    마우스 클릭/드래그/릴리즈로 기본적인 선을 생성할 수 있다.

--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class MouseInput : MonoBehaviour
 {
     private Camera cam;
+    public LineManager lineManager;
 
-    public GameObject holdingObj;
-    public bool isHolding;
+    public Line linePrefab;
+    private Line line_onMaking;
+    private LineRenderer lineRenderer;
+
+    private bool isHolding = false;
 
     void Start()
     {
@@ -16,36 +19,68 @@ public class MouseInput : MonoBehaviour
     void Update()
     {
         var point = cam.ScreenToWorldPoint(Input.mousePosition);
+        var hit = Physics2D.Raycast(point, Vector2.zero);
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))    // 클릭
         {
-            var hit = Physics2D.Raycast(point, Vector2.zero);
             if (hit.collider != null)   // 해당 위치에 충돌한 오브젝트가 존재하는 경우
             {
                 if (hit.collider.CompareTag("Station"))
                 {
-                    Debug.Log("Station");
-                    holdingObj = hit.collider.gameObject;
+                    Debug.Log("선 생성");
+                    var pos = hit.collider.gameObject.transform.position;
+                    pos.z = 0f;
+
+                    line_onMaking = Instantiate(linePrefab, lineManager.transform);
+                    line_onMaking.AddStationEnd(hit.collider.gameObject.GetComponent<Station>());
+
+                    isHolding = true;
+
+                    lineRenderer = line_onMaking.GetComponent<LineRenderer>();
+                    lineRenderer.SetPosition(0, pos);
                 }
-            }
-            else
-            {
-                Debug.Log("충돌 collider 없음");
             }
         }
 
         if (isHolding)
         {
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButton(0))    // 드래그
             {
-                Debug.Log("눌려 있음");
-                holdingObj.transform.position = point;
+                var previewPoint = point;
+                previewPoint.z = 0f;
+                lineRenderer.SetPosition(line_onMaking.stations.Count, previewPoint);
+
+                if (hit.collider != null)   // 해당 위치에 충돌한 오브젝트가 존재하는 경우
+                {
+                    if (hit.collider.CompareTag("Station"))
+                    {
+                        var station = hit.collider.GetComponent<Station>();
+
+                        if (!line_onMaking.stations.Contains(station))
+                        {
+                            line_onMaking.AddStationEnd(station);
+                            Debug.Log($"선에 역 추가: {station.name}");
+                        }
+                    }
+                }
             }
 
-            if (Input.GetMouseButtonUp(0))
+            if (Input.GetMouseButtonUp(0))  // 릴리즈
             {
-                Debug.Log("올라감");
-                holdingObj = null;
+                if (line_onMaking.stations.Count < 2)
+                {
+                    Debug.Log($"미완성 선 삭제");
+                    Destroy(line_onMaking.gameObject);
+                    line_onMaking = null;
+                    isHolding = false;
+                    return;
+                }
+
+                // 선 확정
+                Debug.Log($"선 확정");
+                line_onMaking.Init();
+                line_onMaking = null;
+                isHolding = false;
             }
         }
     }
