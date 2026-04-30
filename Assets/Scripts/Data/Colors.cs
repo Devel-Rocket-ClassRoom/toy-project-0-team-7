907bb1f22f301fc174d9d19111aa13f251c3414c
jinag8904 <jinag8904@gmail.com>
2026-04-30 16:14:17 +0900

    노선 색 만드는 순서대로 적용
    - static Colors 클래스 추가
        - static colors 색 배열 추가
    - 노선을 만들 때 색상이 적용됩니다.
    - 노선 수는 7개를 초과하지 않습니다.

---
+++ Assets/Scripts/Data/Colors.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public static class Colors
+{
+    // 노 빨 파 청 녹 분 갈
+    public static Color[] colors = { Color.yellow, Color.red, Color.blue, Color.cyan, Color.green, Color.pink, Color.brown };
+}
