a35b9420980e9ba1d03746133d342aa2df51dd98
jinag8904 <jinag8904@gmail.com>
2026-05-01 17:06:18 +0900

    #27 노선 편집 - 노선 연장
    노선 양쪽 끝의 손잡이를 당겨 노선 연장 가능

---
+++ Assets/Scripts/Line/Handle.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public class Handle : MonoBehaviour
+{
+    public Line line;
+    public bool isStartHandle;
+
+    public GameObject sero;
+    public GameObject garo;
+
+    private LineRenderer lrSero;
+    private LineRenderer lrGaro;
+
+    public void Awake()
+    {
+        lrSero = sero.GetComponent<LineRenderer>();
+        lrGaro = garo.GetComponent<LineRenderer>();
+    }
+
+    public void SetHandleDirection(Vector3 dir)
+    {
+        var perp = new Vector3(-dir.y, dir.x, 0); // 수직 방향
+
+        // 세로 막대
+        lrSero.SetPosition(0, transform.position);
+        lrSero.SetPosition(1, transform.position + dir * 0.5f);
+
+        // 가로 막대
+        lrGaro.SetPosition(0, transform.position + dir * 0.5f - perp * 0.3f);
+        lrGaro.SetPosition(1, transform.position + dir * 0.5f + perp * 0.3f);
+    }
+
+    public void SetColor(Color color)
+    {
+        if (lrSero == null) lrSero = sero.GetComponent<LineRenderer>(); // 방어 코드
+        if (lrGaro == null) lrGaro = garo.GetComponent<LineRenderer>();
+
+        lrSero.startColor = color;
+        lrSero.endColor = color;
+        lrGaro.startColor = color;
+        lrGaro.endColor = color;
+    }
+}
