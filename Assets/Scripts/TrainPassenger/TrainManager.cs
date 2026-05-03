7229be80595595bd68e7c16d8edec9915999ef27
윤지수 <yoonjisu1201@gmail.com>
2026-05-03 14:20:53 +0900

    #38, #45
    종점역 승객 안태워지는 버그 수정, 무정차 통과시 속도가 일정하게 안나옴(버그로 등록)

--- Assets/Scripts/TrainPassenger/TrainManager.cs
+++ Assets/Scripts/TrainPassenger/TrainManager.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 public class TrainManager : MonoBehaviour
 {
     public List<Train> activeTrains = new List<Train>();
-    //public Train train;
     public GameObject trainPrefab;
-    
 
-    // [수정 예정] 나중에 LineManager가 완성되면 testStations 대신 
-    // 실제 Line이 보유한 Station 리스트를 가져와야 함
+
+    // LineManager에서 Line이 보유한 Station 리스트를 가져옴
     public List<Station> testStations = new();
 
-    private void Start()
-    {
-        //SpawnTrain();
-    }
     private void Update()
     {
         //[체크] 일시정지 기능 추가시 여기서 체크
@@ -26,12 +20,13 @@ public class TrainManager : MonoBehaviour
     }
     public Train SpawnTrain(int lineId)
     {
+        if (testStations.Count == 0) return null;
         //[수정 예정] 실제 게임에선 0번 역이 아니라 사용자가 클릭한 노선의 시작점에 스폰
         GameObject trainObject = Instantiate(trainPrefab, testStations[0].transform.position, Quaternion.identity);
         Train train = trainObject.GetComponent<Train>();
 
         //[수정 예정] 실제 데이터로 변경
-        train.SetPath(testStations);
+        train.SetPath(new List<Station>(testStations));
         train.lineId = lineId;
         activeTrains.Add(train);
 
