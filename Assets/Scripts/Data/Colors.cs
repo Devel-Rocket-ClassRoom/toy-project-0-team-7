14d36bf2265e990f434e7e9785e77cc607b3fefd
이하늘 <150312623+smflsmfqh@users.noreply.github.com>
2026-05-07 16:37:58 +0900

    [Correct] UI 및 노선 색 수정

--- Assets/Scripts/Data/Colors.cs
+++ Assets/Scripts/Data/Colors.cs
@@ -3,5 +3,5 @@ using UnityEngine;
 public static class Colors
 {
     // 노 빨 파 청 녹 분 갈
-    public static Color[] colors = { Color.yellow, Color.red, Color.blue, Color.cyan, Color.green, Color.pink, Color.brown };
+    public static Color[] colors = { Color.gold, Color.tomato, Color.royalBlue, Color.deepSkyBlue, Color.limeGreen, Color.mediumVioletRed, Color.sienna };
 }
